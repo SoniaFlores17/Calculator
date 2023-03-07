07c792d8c6cc3c0fdf12f6d93b825b34da9cd1a6
Sonia Flores Zuñiga <zunigafloressonia105@gmail.com>
2023-03-07 10:03:34 -0600

    Estilos de botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import './App.css'
 function App() {
 
   return (
     <div>
-   <h1>Calculator </h1>
-   <table style={{width:'400px'}}>
-    <tr>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-    </tr>
-    <tr>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-    </tr>
-    <tr>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-    </tr>
-    <tr>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-      <td style={{border:'1px solid black'}}>0</td>
-    </tr>
-   </table>
- </div>
+      <h1>Calculadora</h1>
+      <table>
+        {/* Primer fila*/}
+        <tr>
+          <td colSpan={4}>Resultado</td>
+        </tr>
+        {/* Segunda fila*/}
+        <tr>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              C
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              /
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              *
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              -
+            </button>
+          </td>
+        </tr>
+        {/* Tercer fila*/}
+        <tr>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              7
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              8
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              9
+            </button>
+          </td>
+          <td rowSpan={2}>
+            <button type="button"
+            className="btn btn-primary w-75"
+            style={{height: "80px"}}>
+              +
+            </button>
+          </td>
+        </tr>
+        {/* Cuarta fila*/}
+        <tr>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              4
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              5
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              6
+            </button>
+          </td>
+        </tr>
+        {/* Quinta fila*/}
+        <tr>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              1
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              2
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              3
+            </button>
+          </td>
+          <td rowSpan={2}>
+            <button type="button"
+            className="btn btn-primary w-75"
+            style={{height: "80px"}}>
+              =
+            </button>
+          </td>
+        </tr>
+        {/* Sexta fila*/}
+        <tr>
+          <td colSpan={2}>
+            <button type="button"
+            className="btn btn-primary "
+            style={{width: "175px"}}>
+              0
+            </button>
+          </td>
+          <td>
+            <button type='button'
+              className='btn btn-primary w-75'>
+              .
+            </button>
+          </td>
+        </tr>
+      </table>
+    </div>
   )
 }
 
