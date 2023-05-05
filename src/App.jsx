436e68b70c871eb2b39604a5aca82e3820a0f9aa
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2023-05-05 15:03:05 -0600

    Botones componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
 import { useState } from 'react';
 import './App.css'
+import Button from './Components/Button';
+
 
 const buttonsClases = "btn btn-primary w-75";
 
 function App() {
-  const [operator, setOperator]= useState('');
-  const [number1, setNumber1] = useState('');
+  const [operator, setOperator]= useState("");
+  const [number1, setNumber1] = useState("");
   const [clearScreen, setClearScreen]=useState(false);
-  const [screen, setScreen] = useState('0');
+  const [screen, setScreen] = useState("0");
 
   const handleButtonClick= (e)=> {
     const {value}= e.target;
     if (value==='.'){
       if (screen.includes('.')) return;
     }
-    if(value === 'C'){
-      setScreen('0')
+    if(value === "C"){
+      setScreen("0");
       return;
     }
     //!================================
-    console.log({number1});
+   
     if (clearScreen){
-      console.log('Cambia screen');
       setScreen(value);
       setClearScreen(false);
       return;
     }
-    if (screen === '0' && value !=='.'){
+    if (screen === "0" && value!=='.'){
      setScreen(value);
      return;
     }
-     setScreen(`${screen}${value}`);
+     setScreen(`${screen}${value}`)
   }
 
 
   const handleDelButtonClick = () =>{
     if (screen.length===1){
-      setScreen('0');
+      setScreen("0");
       return;
     }
     setScreen(screen.slice(0, -1));
@@ -53,187 +54,219 @@ function App() {
     const a = +number1;
     const b = +screen;
     switch (operator) {
-      case '+':
+      case "+":
         setScreen((a + b).toString());
         break;
 
+        case "-":
+          setScreen((a-b).toString());
+          break;
+  
+        case "*":
+          setScreen((a*b).toString());
+          break;
+  
+        case "/":
+          setScreen((a/b).toString());
+          break;
         default:
-        break;
+          break;
         
     }
   }
+  
+
+
+  const buttonsInfo = [
+    [
+      {
+        classes: buttonClasses,
+        value: 'C',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '/',
+        handleClick: handleOperationButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '*',
+        handleClick: handleOperationButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '-',
+        handleClick: handleOperationButtonClick,
+        style: {},
+        rows: 1
+      }
+    ],
+    //Segunda fila
+    [
+      {
+        classes: buttonClasses,
+        value: '7',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '8',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '9',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '+',
+        handleClick: handleOperationButtonClick,
+        style: {height:"80px"},
+        rows: 2
+      }
+    ],
+    //Tercer fila
+    [
+      {
+        classes: buttonClasses,
+        value: '4',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '5',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '6',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+    ],
+    //Cuarta fila
+    [
+      {
+        classes: buttonClasses,
+        value: '1',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '2',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '3',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '=',
+        handleClick: handleEqualButtonClick,
+        style: {height:"80px"},
+        rows: 2
+      }
+    ],
+    //Quinta fila
+    [
+      {
+        classes: buttonClasses,
+        value: 'DEL',
+        handleClick: handleDelButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '0',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+
+      {
+        classes: buttonClasses,
+        value: '.',
+        handleClick: handleButtonClick,
+        style: {},
+        rows: 1
+      },
+    ],
+  ]
 
   return (
-    <div className='app'>
-
-      
-      <h1 className='shadow-sm'>Calculadora</h1>
+    <div className="app">
+      <h1 className="shadow-sm">Calculadora</h1>
       <table>
         {/* Primer fila*/}
         <tr>
-          <td colSpan={4} style=
-          {{border:"1px solid black",
-          textAlign: "end"
+          <td colSpan={4} style={{
+            border:"1px solid black",
+            textAlign:"end"
           }}>
             <h2>{screen}</h2>
-            </td>
+          </td>
         </tr>
         {/* Segunda fila*/}
-        <tr>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="C"
-              onClick={(e)=> handleButtonClick(e)} 
-              >C
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}>
-              /
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}>
-              *
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}>
-              -
-            </button>
-          </td>
-        </tr>
-        {/* Tercer fila*/}
-        <tr>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="7"
-              onClick={(e)=> handleButtonClick(e)}
-              >7
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="8"
-              onClick={(e)=> handleButtonClick(e)}
-              >8
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="9"
-              onClick={(e)=> handleButtonClick(e)}
-             >9
-            </button>
-          </td>
-          <td rowSpan={2}>
-            <button 
-            type="button"
-            className={buttonsClases}
-            style={{height: "80px"}}
-            value="+"
-            onClick={(e)=>handleOperationButtonClick(e)}
-            >+
-            </button>
-          </td>
-        </tr>
-        {/* Cuarta fila*/}
-        <tr>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="4"
-              onClick={(e)=> handleButtonClick(e)}
-              >4
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="5"
-              onClick={(e)=> handleButtonClick(e)}
-              >5
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="6"
-              onClick={(e)=> handleButtonClick(e)}
-              >6
-            </button>
-          </td>
-        </tr>
-        {/* Quinta fila*/}
-        <tr>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="1"
-              onClick={(e)=> handleButtonClick(e)}
-              >1
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="2"
-              onClick={(e)=> handleButtonClick(e)}
-              >2
-            </button>
-          </td>
-          <td>
-            <button type='button'
-              className={buttonsClases}
-              value="3"
-              onClick={(e)=> handleButtonClick(e)}
-              >
-              3
-            </button>
-          </td>
-          <td rowSpan={2}>
-            <button type="button"
-            className={buttonsClases}
-            style={{height: "80px"}}
-            onClick={handleEqualButtonClick}
-            >
-              =
-            </button>
-          </td>
-        </tr>
-        {/* Sexta fila*/}
-        <tr>
-          <td>
-            <button 
-            type="button"
-            className={buttonsClases}
-            onClick={handleDelButtonClick}
-            >DEL</button>
-          </td>
-          <td>
-            <button 
-            type="button"
-            className={buttonsClases}
-            value="9"
-            onClick={(e)=> handleButtonClick(e)}
-            >0</button>
-          </td>
-          <td>
-            <button 
-            type="button"
-              className={buttonsClases}
-              value="."
-              onClick={(e)=> handleButtonClick(e)}
-            >.</button>
-          </td>
-        </tr>
+        {
+          buttonsInfo.map((row) => {
+            return(
+              <tr>
+                {
+                row.map((cell) => {
+                  return(
+                    <td rowSpan={cell.rows}>
+                      <Button
+                        classes={cell.classes}
+                        value={cell.value}
+                        handleClick = {cell.handleClick}
+                        style= {cell.style}
+                      />
+                  </td>)
+                })}
+              </tr>
+            )
+          })
+        }
       </table>
     </div>
   )
